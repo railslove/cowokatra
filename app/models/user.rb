30d1dfae38d1870bfe9ccb052033b8bc6e03306c
Maxim Küpper <maxim@railslove.com>
2016-03-16 10:39:42 +0100

    model init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :orders
+  has_many :remaining_orders, -> { where deducted: false }, class_name: 'Order'
+  has_many :deducted_orders, -> { where deducted: true }, class_name: 'Order'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:orders) }
+  it { should have_many(:deducted_orders) }
+  it { should have_many(:remaining_orders) }
 end
