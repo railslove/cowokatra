3f86dd6d4fffdf0d3f67bea6dead5489bf4dfc03
Maxim Küpper <maxim@railslove.com>
2016-03-29 00:32:42 +0200

    date mumbo jumbo

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,20 @@
 class ReportController < ApplicationController
   def global
     @users = UserReportingDecorator.decorate_collection User.order(first_name: :asc)
+    @reporting_weeks = reporting_weeks
+  end
+
+  def current_cweek
+    week = params.fetch(:week, Date.today.cweek).to_i
+    year = params.fetch(:year, Date.today.year).to_i
+    Date.commercial(year, week, 1)
+  rescue
+    Date.today.cweek
+  end
+
+  def reporting_weeks
+    Array.new(3) do |i|
+      (current_cweek - i.weeks)
+    end.reverse
   end
 end
