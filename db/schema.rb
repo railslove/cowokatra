334c7b1b12f335a39001e9b69a41725072f4fe33
Maxim Küpper <maxim@railslove.com>
2016-04-05 14:47:11 +0200

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320115103) do
+ActiveRecord::Schema.define(version: 20160405121546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160320115103) do
     t.decimal  "amount",     precision: 6, scale: 2
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
+    t.datetime "booked_at"
   end
 
   add_index "payments", ["user_id"], name: "index_payments_on_user_id", using: :btree
